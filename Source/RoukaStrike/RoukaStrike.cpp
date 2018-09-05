eb031ea8d977592e9c1e42a6518e69d2965564d8
Unknown <louis.jahn@epitech.eu>
2018-09-05 17:09:27 +0200

    Forgot files

---
+++ Source/RoukaStrike/RoukaStrike.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RoukaStrike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RoukaStrike, "RoukaStrike" );
---
+++ Source/RoukaStrike/RoukaStrikeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RoukaStrikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROUKASTRIKE_API ARoukaStrikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
